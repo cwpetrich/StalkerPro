49d225dd0272f8edfe28889153cd84cf4e096c86
Conrad Petrich <cpetrich@dmail.dixie.edu>
2015-04-25 18:19:16 -0600

    changed the title CreateNote from BlankNote to Create Note.

--- app/src/main/java/com/example/cs3010/stalkerpro/CreateNote.java
+++ app/src/main/java/com/example/cs3010/stalkerpro/CreateNote.java
@@ -5,7 +5,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -84,7 +83,7 @@ public class CreateNote extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_blank_note, menu);
+        getMenuInflater().inflate(R.menu.menu_create_note, menu);
         return true;
     }
 
